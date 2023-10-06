f238fe370b94144fc9b550c62f0383504f33bf04
byhaqii <rizalbaihaqiiiiii@gmail.com>
2023-10-06 14:02:46 +0700

    Commit 7

--- Selection2Exp319.java
+++ Selection2Exp319.java
@@ -11,7 +11,8 @@ public class Selection2Exp319 {
         System.out.print("Input income = ");
         income = input19.nextInt();
 
-        if(category.equalsIgnoreCase("worker")) {
+        if(category.equals("worker")) {
+        //if(category.equals("worker")) {        
             if(income <= 2000000)
                 tax = 0.1;
             else if(income <= 3000000)
@@ -20,7 +21,7 @@ public class Selection2Exp319 {
                 tax = 0.2;
             netSalary =(int) (income - (tax*income));
             System.out.println("Nett salary = " + netSalary);
-        }else if (category.equalsIgnoreCase("businessman")) {
+        }else if (category.equals("businessman")){
             if(income <= 2500000)
                 tax = 0.15;
             else if(income <= 3500000)

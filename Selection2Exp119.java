f0f15918dff33fc1db2d08c35ebe862d083a02ac
byhaqii <rizalbaihaqiiiiii@gmail.com>
2023-10-06 11:05:45 +0700

    Commit 1

---
+++ Selection2Exp119.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Selection2Exp119 {
+    public static void main(String[] args) {
+        Scanner input19 = new Scanner(System.in);
+        System.out.print("Input Year: ");
+        int year = input19.nextInt();
+        if(year%4 == 0)
+            if(year%100 != 0)
+                    System.out.println("Leap Year");
+        else
+            System.out.println("Not A Leap Year");
+    }
+}

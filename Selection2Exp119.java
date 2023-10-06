493dac7aaa8a0cfddc0fd03e8d433b9e2f320015
byhaqii <rizalbaihaqiiiiii@gmail.com>
2023-10-06 11:07:16 +0700

    commitTes

--- Selection2Exp119.java
+++ Selection2Exp119.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class Selection2Exp119 {
     public static void main(String[] args) {
-        Scanner input19 = new Scanner(System.in);
+        Scanner input19 = new Scanner(System.in);//majid anjir
         System.out.print("Input Year: ");
         int year = input19.nextInt();
         if(year%4 == 0)
